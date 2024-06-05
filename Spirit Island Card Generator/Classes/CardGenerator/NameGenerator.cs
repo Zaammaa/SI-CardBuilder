a883fb80abfa8563d291b4f08731f9cf18b8d6c6
krswanson <kristin@swanson.name>
2024-06-05 10:50:20 -0700

    Added initial word associations for card names

--- Spirit Island Card Generator/Classes/CardGenerator/NameGenerator.cs
+++ Spirit Island Card Generator/Classes/CardGenerator/NameGenerator.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Spirit_Island_Card_Generator.Classes.CardGenerator
 {
     public class NameGenerator
     {
-        private Dictionary<string, List<string>> wordLists = new Dictionary<string, List<string>>();
+        private List<string> keywords;
+        private Dictionary<string, Dictionary<string, List<string>>> wordLists = new Dictionary<string, Dictionary<string, List<string>>>();
+        private Dictionary<string, List<string>> masterWordList = new Dictionary<string, List<string>>();
         private Dictionary<string, List<List<string>>> templates = new Dictionary<string, List<List<string>>>();
         private List<string> previousNames = new List<string>();
         private string cardNameDir;
         private Random rng;
+        private string GENERIC_KEY = "Generic";
 
         public NameGenerator(Settings settings)
         {
             cardNameDir = settings.cardNamePath.Equals("") ? Path.Combine(Environment.CurrentDirectory, $"..\\..\\..\\Card Name Words") : settings.cardNamePath;
             rng = settings.rng;
+            keywords = getFileLineList("Association Keywords");
             SetupCardNameOptions();
             SetupTemplateOptions();
         }
@@ -26,18 +31,47 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
             do
             {
                 name = "";
+                var assoList = getElementsAndEffects(card);
                 foreach (var tWord in template)
                 {
                     if (wordLists.ContainsKey(tWord))
                     {
-                        //TODO look at elements/effects to inform word choice
-                        roll = (int)(rng.NextDouble() * wordLists[tWord].Count);
-                        name += wordLists[tWord][roll];
+                        
+                        string word = "";
+                        roll = (int)(rng.NextDouble() * assoList.Count);
+                        string asso = assoList[roll];
+                        Debug.WriteLine("*********************************");
+                        Debug.WriteLine("Part of speech: " + tWord + " Chosen asso: " + asso);
+                        if (wordLists[tWord].ContainsKey(asso))
+                        {
+                            do
+                            {
+                                Debug.WriteLine("Using asso: " + asso);
+                                if (wordLists[tWord][asso].Count > 0)
+                                {
+                                    roll = (int)(rng.NextDouble() * wordLists[tWord][asso].Count);
+                                    word = wordLists[tWord][asso][roll];
+                                    Debug.WriteLine("Found: " + word);
+                                    wordLists[tWord][asso].Remove(word);
+                                }
+                                else {
+                                        word = selectGenericWord(tWord);
+                                        Debug.WriteLine("Have to use Generic, got " + word);
+                                    }
+                            } while (!masterWordList[tWord].Contains(word));
+                        }
+                        else
+                        {
+                           selectGenericWord(tWord);
+                        }
+                        Debug.WriteLine("--Chose: " + word);
+                        name += word;
                         //TODO? worth fixing that words will still be removed if name was too long initially
-                        wordLists[tWord].RemoveAt(roll);
-                        if (wordLists[tWord].Count == 0)
+                        masterWordList[tWord].Remove(word);
+                        if (masterWordList[tWord].Count == 0)
                             SetupCardNameOptions(tWord.Replace("_", ""));
-                    } else
+                    }
+                    else
                         name += tWord;
                     name += " ";
                 }
@@ -45,6 +79,43 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
             } while (previousNames.Contains(name) || name.Length > 40);
             previousNames.Add(name);
             return name;
+        }
+
+        private List<string> getElementsAndEffects(Card card)
+        {
+            var assoList = new List<string>();
+            var effectList = new List<string>();
+            var selectionList = new List<string>();
+            foreach (var el in card.elements.GetElements())
+                assoList.Add(el.ToString());
+            foreach (var ef in card.effects)
+            {
+                foreach (var asso in keywords)
+                {
+                    string efStr = ef.GetType().Name;
+                    if (efStr.Contains(asso))
+                        assoList.Add(asso);
+                }
+            }
+            return assoList;
+        }
+
+        private string selectGenericWord(string wordType)
+        {
+            string word = "";
+            if (wordLists[wordType][GENERIC_KEY].Count > 0)
+            {
+                int roll = (int)(rng.NextDouble() * wordLists[wordType][GENERIC_KEY].Count);
+                word = wordLists[wordType][GENERIC_KEY][roll];
+                wordLists[wordType][GENERIC_KEY].Remove(word);
+            }
+            else
+            {
+                Debug.WriteLine("############## Resorted to masterList ###################");
+                int roll = (int)(rng.NextDouble() * masterWordList[wordType].Count);
+                word = masterWordList[wordType][roll];
+            }
+            return word;
         }
 
         private List<string> selectTemplate(Card card)
@@ -57,7 +128,7 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
                 type = "Spirit-Targeting";
             if (target.Contains("dahan") && roll < 75)
                 type = "Dahan-Targeting";
-            int tType = (int)(rng.NextDouble()*100);
+            int tType = (int)(rng.NextDouble() * 100);
             roll = (int)(rng.NextDouble() * templates[type].Count);
             var template = templates[type][roll];
             templates[type].RemoveAt(roll);
@@ -69,14 +140,38 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
         private void SetupCardNameOptions(string singleType = "no")
         {
             var types = new List<String>() { "Nouns", "Adjectives", "Verbs", "Possessives", "SILocations" };
-            if ( singleType != "no")
+            if (singleType != "no")
             {
                 types.Clear();
                 types.Add(singleType);
             }
             foreach (var type in types)
             {
-                wordLists[$"__{type}__"] = File.ReadAllLines(Path.Combine(cardNameDir, $"{type}.txt")).ToList();
+                string typeKey = $"__{type}__";
+                masterWordList[typeKey] = new List<string>();
+                wordLists[typeKey] = new Dictionary<string, List<string>>();
+                wordLists[typeKey][GENERIC_KEY] = new List<string>();
+                foreach (string assoKey in keywords)
+                {
+                    wordLists[typeKey][assoKey] = new List<string>();
+                }
+                var lineList = getFileLineList(type);
+                foreach (var line in lineList)
+                {
+                    var wordAndType = line.Split("#");
+                    var word = wordAndType[0].Trim();
+                    if (wordAndType.Length == 2)
+                    {
+                        var assoList = wordAndType[1].Trim().Split(" ");
+                        foreach (string asso in assoList)
+                        {
+                            wordLists[typeKey][asso.Trim()].Add(word);
+                        }
+                    }
+                    else
+                        wordLists[typeKey][GENERIC_KEY].Add(word);
+                    masterWordList[typeKey].Add(word);
+                }
             }
         }
 
@@ -90,7 +185,7 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
             }
             foreach (var type in types)
             {
-                var lines = File.ReadAllLines(Path.Combine(cardNameDir, $"{type} Templates.txt")).ToList();
+                var lines = getFileLineList($"{type} Templates");
                 var templateFile = new List<List<string>>();
                 foreach (var line in lines)
                 {
@@ -110,6 +205,11 @@ namespace Spirit_Island_Card_Generator.Classes.CardGenerator
                 }
                 templates[type] = templateFile;
             }
+        }
+
+        private List<string> getFileLineList(string filename)
+        {
+            return File.ReadAllLines(Path.Combine(cardNameDir, $"{filename}.txt")).ToList();
         }
     }
 }
